009b3e6a8d84f372700c5746498a63e32281116c
haddjs <nadirhexonyte@gmail.com>
2025-10-27 23:09:56 +0700

    feat: fixing clock component

--- src/components/common/Clock.jsx
+++ src/components/common/Clock.jsx
@@ -4,30 +4,42 @@ const Clock = () => {
 	const [time, setTime] = useState(new Date());
 
 	useEffect(() => {
-		const tick = () => setTime(new Date());
-		const timerId = setInterval(tick, 1000);
-
-		return () => clearInterval(timerId);
+		const interval = setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+		return () => clearInterval(interval);
 	}, []);
 
-	const formatTime = time.toLocaleString("id-ID", {
+	const timeFormat = time.toLocaleString("id-ID", {
 		hour: "numeric",
 		minute: "numeric",
 		hour12: true,
 	});
 
+	const clockReminder = timeFormat.split(":")[0];
+	const dayNightReminder = timeFormat.split(" ")[1];
+
+	const reminder =
+		(dayNightReminder === "PM" && clockReminder >= 10) ||
+		(dayNightReminder === "AM" && clockReminder >= 12) ||
+		clockReminder < 6;
+
 	return (
 		<div className="bg-white/20 py-3 px-5 w-50 rounded-md ring-2 ring-white/25">
 			<h1 className="font-bold text-black">
-				{formatTime > "10.00PM" && (
-					<div className="flex flex-col gap-3">
-						<h1>{formatTime}</h1>
-						<span className="font-medium">
-							Sleep, <br />
-							Enough coding for today.
-						</span>
-					</div>
-				)}
+				<div className="flex flex-col gap-3">
+					<h1>{timeFormat}</h1>
+					<span className="font-medium">
+						{reminder ? (
+							<p>
+								Sleep, <br />
+								Enough coding for today.
+							</p>
+						) : (
+							<p>Let's do some code!</p>
+						)}
+					</span>
+				</div>
 			</h1>
 		</div>
 	);
